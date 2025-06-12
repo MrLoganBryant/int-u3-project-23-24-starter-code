61064d75b38ad260ff83dca010bddb9eb970dd9b
MrLoganBryant <mrloganbryant26@gmail.com>
2025-06-12 11:45:45 -0500

    base working adding more

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-// Declare variables below to save the different divs of your story.
-
-
-
-
-
-
-
-
-
-// When you're ready to make event handlers, uncomment the code below. 
-//  - Then fill in the blanks with the correct variables.
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+function chooseOption(direction) {
+      const story = document.getElementById("story");
+      if (direction === 'left') {
+        story.innerHTML = "You walk into the swamp. The swamp is silent... until something moves. A shadow approaches.";
+      } else if (direction === 'right') {
+        story.innerHTML = "You continue into the forest. A path leads down to a simple cottage.";
+      }
+      // change the photo to the back ground 
+    }
